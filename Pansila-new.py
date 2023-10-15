ad32d9d144c8bcf2ff72a0ab2b985bc95c81d40d
Pansila <hansaniliyanage17@gmail.com>
2023-10-15 12:36:56 +0530

    Added new code to convert meters into centimeters

---
+++ Pansila-new.py
@@ -0,0 +1,9 @@
+#Code to takes a length in meters as input from the user and converts it to centimeters
+# Input length in meters from the user
+meters = float(input("Enter length in meters: "))
+
+# Convert meters to centimeters
+centimeters = meters * 100
+
+# Print the result
+print(f"{meters} meters is equal to {centimeters} centimeters.")
