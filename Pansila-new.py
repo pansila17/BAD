0c9da42a23a787952d2816433ee2e3a49aa56ea6
Pansila <hansaniliyanage17@gmail.com>
2023-10-15 14:02:10 +0530

    Added new code to calculate the area of a rectangle

--- Pansila-new.py
+++ Pansila-new.py
@@ -7,3 +7,14 @@ centimeters = meters * 100
 
 # Print the result
 print(f"{meters} meters is equal to {centimeters} centimeters.")
+
+# code to calculate the area of a rectangle based on user input for its length and width
+# Input from the user for length and width
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Print the result
+print(f"The area of the rectangle with length {length} and width {width} is {area}.")
