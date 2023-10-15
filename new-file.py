ad9164d07921d69473a547de00805ebc95668232
Pansila <hansaniliyanage17@gmail.com>
2023-10-15 22:16:12 +0530

    Current date and time

--- new-file.py
+++ new-file.py
@@ -3,3 +3,9 @@ name = input("Enter your name: ")
 
 # Greet the user
 print(f"Hello, {name}! Nice to meet you.")
+
+# current date and time
+from datetime import datetime
+
+current_time = datetime.now()
+print("Current date and time:", current_time)
