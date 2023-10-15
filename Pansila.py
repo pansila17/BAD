0aeb0f9abd35f0c551cf996427a6a6a3f63bf248
Pansila <hansaniliyanage17@gmail.com>
2023-10-15 11:50:31 +0530

    Added new code to check whether a number is prime or not

--- Pansila.py
+++ Pansila.py
@@ -8,3 +8,30 @@ sum_result = num1 + num2
 
 # Display the result
 print(f"The sum of {num1} and {num2} is {sum_result}")
+
+# Check if a number is prime
+# Function to check if a number is prime
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+# Input from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is prime
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
