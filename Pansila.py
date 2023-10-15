cc96fa241dfb9869456794cb5c4743bf5d55c969
Pansila <hansaniliyanage17@gmail.com>
2023-10-15 12:00:56 +0530

    Added new code to get a list of all the prime numbers between 1 and 100

--- Pansila.py
+++ Pansila.py
@@ -35,3 +35,19 @@ if is_prime(number):
     print(f"{number} is a prime number.")
 else:
     print(f"{number} is not a prime number.")
+
+# Code  to get a list of all the prime numbers between 1 and 100
+
+def is_prime(number):
+  for i in range(2, int(number**0.5) + 1):
+    if number % i == 0:
+      return False
+  return True
+
+
+prime_numbers = []
+for number in range(1, 101):
+  if is_prime(number):
+    prime_numbers.append(number)
+
+print(prime_numbers)
